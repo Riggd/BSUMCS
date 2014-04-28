80d4b4e9598ffae70d20ade87eedde985bab3db1
Derek Onay <wr3tched@gmail.com>
2014-04-28 11:42:10 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>BSU Men's Club Soccer</title>
+</head>
+<body>
+    
+    <header id="header">
+        <!-- Nav Here -->
+        <div class="container">
+            <a href="">
+                <!-- LOGO HERE -->
+                <img src="">
+            </a>
+            <nav>
+                <ul>
+                    <li>About Us</li>
+                    <li>Team</li>
+                    <li>Venues</li>
+                    <li>Contact Us</li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+    
+    <!-- wrapper then sections then container -->
+    <section id="intro"></section>
+    <section id="about"></section>
+    <section id="team">
+        <section id="roster"></section>
+        <section id="schedule"></section>
+    </section>
+    <section id="venues"></section>
+    <section id="contact"></section>
+    <footer id="footer">
+        &copy; Ball State Men's Club Soccer 2014 | All rights Reserved.
+        <br />
+        design by: Derek Onay
+    </footer>
+</body>
+</html>
